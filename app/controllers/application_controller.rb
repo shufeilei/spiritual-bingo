3b7fdded50a02b742fe712e64bfadc26aea7c1fa
Shufei Lei <shufeilei@berkeley.edu>
2018-05-04 12:06:18 -0700

    Added username to users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_action :authenticate_user!
   protect_from_forgery with: :exception
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :team_id])
+  end
 end
