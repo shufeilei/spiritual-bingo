b9b804930c830abdad52e2b404bcdbdfac4c5a8a
Shufei Lei <shufeilei@berkeley.edu>
2018-05-04 09:33:10 -0700

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users
+  resources :boards do
+    resources :cells
+  end
+  resources :cells
+
   root "pages#show", page: "home"
 
 end
