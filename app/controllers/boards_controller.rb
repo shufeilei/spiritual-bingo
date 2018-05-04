e4f49c9941df15d972fb00633381a56f55ac4281
Shufei Lei <shufeilei@berkeley.edu>
2018-05-04 09:34:38 -0700

    Updated boards controller model and some views

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,39 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: %i[show edit update destroy delete_picture]
+
   def index
+    @boards = Board.include(:user).all
+  end
+
+  def show
+    @cells = @board.cells
+    @owner = @board.user == current_user ? 'Your' : @board.user.possessive
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      flash[:success] = 'Your BINGO board has been created!'
+      redirect_to @board
+    else
+      flash[:error] = 'Oops! Something went wrong. Please try again.'
+      render 'new'
+    end
   end
 
   def new
+    redirect_to current_user.board if current_user.board
+    @board = Board.new
   end
+
+  private
+
+  def set_board
+    @board = Board.find(params[:id])
+  end
+
+  def board_params
+    params.require(:board).permit(:user_id)
+  end
+
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,57 @@
 class Board < ApplicationRecord
-  has_many :cells
+  SCORE = {
+    easy: 5,
+    medium: 10,
+    hard: 15,
+    xhard: 20,
+  }
 
-  
+  TEMPLATE = [
+    [
+      { name: 'blah', level: 'easy', description: 'hey' },
+      { name: 'ha', level: 'hard', description: 'hey' },
+      { name: 'blah', level: 'medium', description: 'hey' },
+      { name: 'ha', level: 'hard', description: 'hey' },
+      { name: 'ka', level: 'easy', description: 'hey' }
+    ],
+    [
+      { name: 'ha', level: 'hard', description: 'hey' },
+      { name: 'blah', level: 'medium', description: 'hey' },
+      { name: 'blah', level: 'easy', description: 'hey' },
+      { name: 'blah', level: 'medium', description: 'hey' },
+      { name: 'ha', level: 'hard', description: 'hey' },
+    ],
+    [
+      { name: 'blah', level: 'medium', description: 'hey' },
+      { name: 'blah', level: 'easy', description: 'hey' },
+      { name: 'ha', level: 'xhard', description: 'REALLY HARD' },
+      { name: 'blah', level: 'easy', description: 'hey' },
+      { name: 'blah', level: 'medium', description: 'hey' },
+    ],
+    [
+      { name: 'ha', level: 'hard', description: 'hey' },
+      { name: 'blah', level: 'medium', description: 'hey' },
+      { name: 'blah', level: 'easy', description: 'hey' },
+      { name: 'blah', level: 'medium', description: 'hey' },
+      { name: 'ha', level: 'hard', description: 'hey' },
+    ],
+    [
+      { name: 'blah', level: 'easy', description: 'hey' },
+      { name: 'ha', level: 'hard', description: 'hey' },
+      { name: 'blah', level: 'medium', description: 'hey' },
+      { name: 'ha', level: 'hard', description: 'hey' },
+      { name: 'ka', level: 'easy', description: 'hey' }
+    ],
+  ]
+
+  has_many :cells
+  belongs_to :user
+
+  def individual_score
+
+  end
+
+  def team_score
+    # sum of all individual scores on the team
+  end
 end
