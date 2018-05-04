1714a540ada24ada67d43b63f16f63c15d79edc2
Shufei Lei <shufeilei@berkeley.edu>
2018-05-04 08:40:16 -0700

    Updated cell controller, model and view

---
+++ app/controllers/cells_controller.rb
@@ -0,0 +1,30 @@
+class CellsController < ApplicationController
+  before_action :set_cell, only: %i[show edit update destroy delete_picture]
+
+  def new
+    @cell = Cell.new
+    @selected_cell = Board::TEMPLATE[params[:row].to_i][params[:col].to_i]
+  end
+
+  def create
+    @cell = Cell.new(cell_params)
+    board = Board.find(params[:board_id])
+    if @cell.save
+      flash[:success] = 'Your BINGO cell has been created!'
+    else
+      flash[:error] = 'Oops! Something went wrong. Please try again.'
+    end
+    redirect_to board
+  end
+
+  private
+
+  def set_cell
+    @cell = Cell.find(params[:id])
+  end
+
+  def cell_params
+    params.require(:cell).permit(:user_id)
+  end
+
+end
