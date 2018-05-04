df70a3ec943fe3257df19fcf869e5ac6d3096d07
Shufei Lei <shufeilei@berkeley.edu>
2018-05-03 22:38:46 -0700

    Updated cell model

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,4 +1,7 @@
 class Cell < ApplicationRecord
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
+  belongs_to :board
+
 end
