cbd80e4a0e0e6c27919b969ea1a1a8992c032e35
Shufei Lei <shufeilei@berkeley.edu>
2018-05-21 10:30:04 -0700

    Auto-orient source file

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,5 +1,9 @@
 class Cell < ApplicationRecord
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture,
+                    source_file_options: { all: '-auto-orient' },
+                    styles: { medium: "300x300>", thumb: "100x100>" },
+                    default_url: "/images/:style/missing.png"
+                    
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
   validates :response, presence: true
