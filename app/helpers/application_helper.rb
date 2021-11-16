4d1c9f79e9ae5a73562e17650426c8f305b0a260
Shufei Lei <shufeilei@berkeley.edu>
2021-11-16 23:59:49 +0000

    fixed flash messages and header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,13 @@ module ApplicationHelper
 
     markdown.render(text).html_safe
   end
+
+  def flash_class(level)
+    case level
+    when "notice" then "alert alert-info"
+    when "error" then "alert alert-danger"
+    when "alert" then "alert alert-warning"
+    else "alert alert-#{level}"
+    end
+  end
 end
